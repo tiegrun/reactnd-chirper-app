4d3ba68a943bdc3f2b5541c0166ddd5c62762388
tiegrun <tiko3020@gmail.com>
2022-02-17 01:10:56 -0500

    w/o routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading-bar'
+import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
   render() {
     return (
       <div>
-        Starter Code
+        <LoadingBar />
+        {this.props.loading === true
+          ? null
+          : <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}} />}
       </div>
     )
   }
 }
 
-export default App
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
